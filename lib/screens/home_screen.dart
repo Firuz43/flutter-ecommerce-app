25e26ec48fc02f6b55a9e5521c17ec39cf1a215e
Firuz Chamarov <firuz1199@mail.ru>
2025-10-12 16:00:48 +0400

    homepage call to api

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,8 +12,35 @@ class _HomeScreenState extends State<HomeScreen> {
   String message = "Loading.."; //Added
   final api = ApiService(); //Added
 
+  //Added
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  //Added
+  Future<void> loadData() async {
+    try {
+      final data = await api.getHello();
+      setState(() => message = data);
+    } catch (e) {
+      setState(() => message = "Error: ${e.toString()}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("E-Commerce App"),
+      ),
+      body: Center(
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+    );
   }
 }
