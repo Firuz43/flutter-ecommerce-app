9aed3ed8a0c12ed991e7fe8c7a0c7d95b06e5fbf
Firuz Chamarov <firuzchamarov@gmail.com>
2025-10-17 15:14:33 +0400

    product model updates

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
+import 'package:ecommerce_app/models/product.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "http://10.0.2.2:8080/api"; //temporary update later
+  final String baseUrl = "http://10.0.2.2:8080/api"; //updated to 10.0.2.2
 
   Future<String> getHello() async {
     //this one Uri.parse will turn out url to http object
@@ -12,6 +15,15 @@ class ApiService {
       return response.body;
     } else {
       throw Exception('Failed to fetch data');
+    }
+  }
+
+  Future<List<Product>> getProducts() async {
+    final url = Uri.parse('$baseUrl/products');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonList = json.decode(response.body);
     }
   }
 }
