fdc578555e42c573213ab64055ca4a732a8f296d
Firuz Chamarov <firuzchamarov@gmail.com>
2025-10-17 15:06:32 +0400

    creating our product model

---
+++ lib/models/product.dart
@@ -0,0 +1,26 @@
+class Product {
+  final int id;
+  final String name;
+  final double price;
+  final String description;
+  final String imageUrl;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.description,
+    required this.imageUrl,
+  });
+
+  // FActory constructor to create a Product from JSON
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      price: (json['price'] as num).toDouble(),
+      description: json['description'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
